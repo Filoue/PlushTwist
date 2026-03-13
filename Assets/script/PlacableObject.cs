c66c7d637f8af23e73f25d0697a49071b3e279e0
Filoue <tonarypetitfiloue@gmail.com>
2026-03-13 20:43:13 +0100

    some new stupid things

--- Assets/script/PlacableObject.cs
+++ Assets/script/PlacableObject.cs
@@ -1,13 +1,51 @@
 ﻿using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 
 public class PlacableObject : MonoBehaviour
 {
+    [Header("Placable Object")]
+    [SerializeField] private GameObject[] prefabs;
     [SerializeField] private Vector3Int position;
+    
+    [Header("player")]
+    [SerializeField] private float speed;
+    [SerializeField] private LayerMask layerMask;
+    
+    private Vector2 mousePosition;
+    private Camera camera;
 
-    private void Update()
+    void Start()
     {
-        
+        camera = Camera.main;
+    }
+
+    public void OnMousePos(InputAction.CallbackContext context)
+    {
+        mousePosition = context.ReadValue<Vector2>();
+    }
+
+    public void OnClick(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            PerformRayCast();
+        }
+    }
+
+    private void PerformRayCast()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(mousePosition);
+
+        if (Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity, layerMask))
+        {
+            Debug.Log($"Hit {hit.transform.name} at {hit.point}");
+        }
+    }
+
+    void Update()
+    {
+        //Debug.Log($"Mouse Screen Position: {mousePosition}");
     }
 }
---
+++ Assets/script/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+
+    public class PlayerController : MonoBehaviour
+    {
+       [SerializeField] private float speed = 10f;
+        private Vector2 inputDirection;
+        private Camera cam;
+
+        public void OnMove(InputAction.CallbackContext context)
+        {
+            inputDirection = context.ReadValue<Vector2>();
+        }
+
+        void Start()
+        {
+            cam =  Camera.main;
+        }
+        void Update()
+        {
+            Vector3 move = new Vector3(inputDirection.x, 0, inputDirection.y) * speed * Time.deltaTime;
+        
+            cam.transform.Translate(move, Space.World);
+        }
+    }
